0aebe3d04e0d2a56244e2cc50f7cf9f6bf740800
Athílio Silva <athilioalt@gmail.com>
2021-07-18 10:14:38 -0300

    confirmação de campos preenchidos no formulario

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,45 @@
 const form = document.getElementById('form');
 
+function empty(element){
+    return !element;
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let nome = document.getElementById('nome').value;
+    let name = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
 
-    let data = {
-        nome,
-        email
+    if(empty(name) || empty(email)) {
+
+        let alert = document.getElementById('alerta');
+        let msgAlert = `
+            <div style="text-align: left">
+                <p>***Preencha todos os campos para realizar o cadastro</p>
+            </div>
+        `;
+        alert.innerHTML = msgAlert;
+
+    } else {
+        let data = {
+            name,
+            email
+        }
+    
+        let convertData = JSON.stringify(data);
+    
+        localStorage.setItem('lead', convertData);
+    
+        let content = document.getElementById('content');
+    
+        let pronto = `
+            <div>
+                <h2 class="msg">Obrigado por se cadastrar</h2>
+                <p>Em breve te mandaremos super ofertas!</p>
+            </div>
+        `;
+    
+        content.innerHTML = pronto;
     }
-
-    let convertData = JSON.stringify(data);
-
-    localStorage.setItem('lead', convertData);
-
-    let content = document.getElementById('content');
-
-    let pronto = `
-        <div>
-            <h2 class="msg">Obrigado por se cadastrar</h2>
-            <p>Em breve te mandaremos super ofertas!</p>
-        </div>
-    `;
-
-    content.innerHTML = pronto;
 
 })
